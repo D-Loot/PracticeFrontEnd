fb7708c13c13ac5dca1a0bc10f0109de81142780
Dan Looten <danwlooten@gmail.com>
2022-02-15 00:41:18 -0600

    fix cloudify attempt 2

--- client/snowpack.config.mjs
+++ client/snowpack.config.mjs
@@ -28,7 +28,7 @@ export default {
     /* ... */
   },
   env: {
-    CLOUDINARY_CLOUD_NAME: "agencyimages",
+    CLOUDINARY_CLOUD_NAME: "dqbaadydi",
     CLOUDINARY_BASE_URL: "https://res.cloudinary.com/AgencyImages/image/upload",
     CLOUDINARY_UPLOAD_PRESET: "agency_images",
   },
